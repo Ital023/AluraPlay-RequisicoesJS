a13025f12333fc056671a34efdf5738558bf8525
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-05-04 17:05:33 -0300

    adicionando algoritmo de criar video

--- js/conectarAPI.js
+++ js/conectarAPI.js
@@ -5,6 +5,22 @@ async function listaVideos(){
     return conexaoConvertida;
 }
 
+async function criarVideo(titulo,descricao,url,imagem){
+    const conexao = await fetch("https://api-videos-4edpvdne6-ital023.vercel.app/videos",{
+        method: "POST",
+        headers:{
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+}
+
 export const conectarAPI = {
-    listaVideos
+    listaVideos,
+    criarVideo
 }
