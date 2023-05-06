3eb3964d66a98e1d1da9c7c102c6c1889ac3f82a
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-05-06 18:29:47 -0300

    Melhorando a api

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,12 +1,19 @@
+import { conectarAPI } from "./conectarAPI.js";
+
 const formulario = document.querySelector("[data-formulario]");
 
-function criarVideo(evento){
+async function criarVideo(evento){
     evento.preventDefault();
     
     const imagem = document.querySelector("[data-imagem]").value;
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectarAPI.criarVideo(titulo,descricao,url,imagem);
+
+    window.location.href = "../pages/envio-concluido.html";
+
 }
 
 formulario.addEventListener("submit",event => criarVideo(evento));
